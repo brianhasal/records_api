08ebb9fad02d64cff2ba7f4ffd0fe3545a011ce3
Brian Hasal <brian.hasal@gmail.com>
2022-05-13 04:55:16 -0700

    Added User to records

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post "/users" => "users#create"
+
+  post "/sessions"=> "sessions#create"
+
+
   get "/records" => "records#index"
   post "/records" => "records#create"
   get "/records/:id" => "records#show"
